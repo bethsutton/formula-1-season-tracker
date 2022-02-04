a728e135ffc79079b0f2bcf9a074871c65126a43
Beth Sutton <bksutton2@gmail.com>
2022-02-03 17:02:29 -0700

    added Footer component

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,10 @@
 module.exports = {
-  content: [
-    "./src/**/*.{js,jsx,ts,tsx}",
-  ],
+  content: ['./src/**/*.{js,jsx,ts,tsx}'],
   theme: {
     extend: {},
   },
   plugins: [require('daisyui')],
   daisyui: {
-    themes: ['dark'],
+    themes: ['bumblebee'],
   },
-}
+};
