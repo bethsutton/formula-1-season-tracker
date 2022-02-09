d3d030047542c360f4d11e2b4b9d18f5e6fa4b31
Beth Sutton <bksutton2@gmail.com>
2022-02-09 15:23:02 -0700

    worked on responsive NavBar

--- src/components/layout/Navbar.jsx
+++ src/components/layout/Navbar.jsx
@@ -6,7 +6,7 @@ import PropTypes from 'prop-types';
 function Navbar({ title }) {
   return (
     <nav className="navbar mb-12 shadow-lg bg-neutral text-neutral-content">
-      <div className="container mx-auto">
+      <div className="container mx-auto flex flex-col md:flex-row">
         <div className="flex-none px-2 mx-2">
           <FaTrophy className="inline pr-2 text-3xl" />
           <Link to="/" className="text-lg font-bold align-middle">
@@ -14,7 +14,7 @@ function Navbar({ title }) {
           </Link>
         </div>
 
-        <div className="flex-1 px-2 mx-2">
+        <div className="flex flex-1 px-2 mx-2 justify-start">
           <div className="flex justify-end">
             <Link to="/" className="btn btn-ghost btn-sm rounded-btn">
               Home
